cb39983ad7a3f45a4a4fac7c85cd54ac1f6af6bb
David Morales <dsmorales349@misena.edu.co>
2021-02-03 21:25:52 -0500

    add handlers

--- main.go
+++ main.go
@@ -2,5 +2,8 @@ package main
 
 func main() {
 	server := NewServer(":3000")
+	server.Handle("/", HandleRoot)
+	server.Handle("/home", HandleHome)
 	server.Listen()
+
 }
--- router.go
+++ router.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
@@ -15,6 +14,18 @@ func NewRouter() *Router {
 	}
 }
 
+func (r *Router) FindHandler(path string) (http.HandlerFunc, bool) {
+	handler, exist := r.rules[path]
+	return handler, exist
+}
+
 func (r *Router) ServeHTTP(w http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(w, "Hola mundo")
+	handler, exist := r.FindHandler(request.URL.Path)
+
+	if !exist {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	handler(w, request)
 }
