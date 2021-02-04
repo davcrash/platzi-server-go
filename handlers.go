b1f764655bfcc101175407ceea0dd453ddcb3478
David Morales <dsmorales349@misena.edu.co>
2021-02-04 10:11:44 -0500

    finish server structure

--- handlers.go
+++ handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -11,4 +12,32 @@ func HandleRoot(w http.ResponseWriter, r *http.Request) {
 
 func HandleHome(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "HOME!")
+}
+func PostRequest(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var metadata MetaData
+	err := decoder.Decode(&metadata)
+	if err != nil {
+		fmt.Fprintf(w, "Error %v", err)
+		return
+	}
+	fmt.Fprintf(w, "Payload %v\n", metadata)
+}
+
+func UserPostRequest(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var user User
+	err := decoder.Decode(&user)
+	if err != nil {
+		fmt.Fprintf(w, "Error %v", err)
+		return
+	}
+
+	response, err := user.toJson()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
 }
--- types.go
+++ types.go
@@ -1,5 +1,20 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 type Middleware func(http.HandlerFunc) http.HandlerFunc
+
+type MetaData interface{}
+
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Phone string `json:"phone"`
+}
+
+func (u *User) toJson() ([]byte, error) {
+	return json.Marshal(u)
+}
